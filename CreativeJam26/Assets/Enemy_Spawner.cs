be671b6d5dbc98d481b100a0f4e45ba6915f274b
Matthew Nichols <matthewnich015@outlook.com>
2026-09-19 11:48:41 -0400

    Enemies working

---
+++ CreativeJam26/Assets/Enemy_Spawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+// SCOPE: spawns enemies from a queue. provides script to populate queue (used by wave manager)
+public class Enemy_Spawner : MonoBehaviour {
+
+    public float spawnRate;
+    private Queue<GameObject> enemySpawnQueue;
+    private float timeSinceSpawn;
+    private Active_Enemy_List_Manager AELM;
+
+    void Awake() {
+        enemySpawnQueue = new Queue<GameObject>();
+        AELM = GetComponent<Active_Enemy_List_Manager>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timeSinceSpawn += Time.deltaTime;
+        if (timeSinceSpawn > spawnRate) {
+            if (enemySpawnQueue.Count == 0) return;
+            timeSinceSpawn = 0;
+            GameObject nextObject = enemySpawnQueue.Dequeue();
+            GameObject newObject = Instantiate(nextObject);
+            AELM.AddEnemyToList(newObject);
+        }
+    }
+
+    public void AddToQueue(GameObject obj) {
+        enemySpawnQueue.Enqueue(obj);
+    }
+}
---
+++ CreativeJam26/Assets/Spline.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Splines;
+
+// SCOPE: allows enemies to find the position they have to move to based on relative progress along spline
+public class Spline : MonoBehaviour
+{
+    SplineContainer splineContainer;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        splineContainer = GetComponent<SplineContainer>();
+    }
+    
+    public Vector3 GetPositionOnSpline(float progress) {
+        Vector3 position = splineContainer.Spline.EvaluatePosition(progress);
+        return position;
+    }
+}
