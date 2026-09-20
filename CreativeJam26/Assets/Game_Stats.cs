3b3a0ee9eab05b94d76c3a45266646ad7dd29a46
You Ran Wang <thatrandomguy666666@gmail.com>
2026-09-20 11:34:12 -0400

    ADD: UI

--- CreativeJam26/Assets/Game_Stats.cs
+++ CreativeJam26/Assets/Game_Stats.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using TMPro;
 public class Game_Stats : MonoBehaviour {
     public static Game_Stats Instance { get; private set; }
     public int Health;
@@ -7,13 +7,25 @@ public class Game_Stats : MonoBehaviour {
     public int Gold1;
     public int Gold2;
     public int Gold3;
-    
+
+    public TextMeshProUGUI gold1Text;
+    public TextMeshProUGUI gold2Text;
+    public TextMeshProUGUI healthText;
+
     private void Awake() {
         if (Instance != null && Instance != this) {
             Destroy(gameObject);
             return;
         }
         Instance = this;
+    }
+
+    void Update()
+    {
+        // Updates every frame; for better performance, call this only when the score changes
+        healthText.text = "HP: " + Health.ToString();
+        gold1Text.text = "Gold: " + Gold1.ToString();
+        gold2Text.text = "Gold: " + Gold2.ToString();
     }
 
 }
