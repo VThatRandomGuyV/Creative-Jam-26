50897d139313187071075319d0bad6decbbe7830
Matthew Nichols <matthewnich015@outlook.com>
2026-09-20 16:12:56 -0400

    projectiles and shaman

--- CreativeJam26/Assets/Scripts/Projectile.cs
+++ CreativeJam26/Assets/Scripts/Projectile.cs
@@ -13,7 +13,14 @@ public class Projectile : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private int projectileDamage = 1;
 
+    public bool rotates;
     private Transform target;
+<<<<<<< Updated upstream
+=======
+    private AudioCueEntry[] soundOverrides;
+    private bool hasHit;
+    private Vector3 lastPosition;
+>>>>>>> Stashed changes
 
     public void SetTarget(Transform _target)
     {
@@ -31,6 +38,18 @@ public class Projectile : MonoBehaviour
         if (!target) return;
         Vector2 direction = (target.position - transform.position).normalized;
         rb.linearVelocity = direction * speed;
+        
+        // set sprite rotation
+        if (rotates) {
+            transform.Rotate(0, 0, 1000 * Time.deltaTime);
+        }
+        else {
+            Vector3 difference = transform.position - lastPosition;
+            float angle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, 0, angle);
+            
+            lastPosition = transform.position;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
